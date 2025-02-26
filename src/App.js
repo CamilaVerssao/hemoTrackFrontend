50cbe568e678f4600fa7bae84e737c2793f0e5c8
Camila <camila.verssao@gmail.com>
2025-02-26 16:53:45 -0300

    Normalizando finais de linha para LF

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // imports dos componentes
+import Header from "./components/Header";
+import Hospital from "./pages/Hospital";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<></>} />
-        <Route path="*" element={<></>} />
-      </Routes>
+      <div className="flex flex-col min-h-screen"> {/* Flex container para o layout */}
+        <Header />
+        <main className="flex-1 mt-[10vh]"> {/* O conteúdo ocupa o restante da tela */}
+          <Routes>
+            {/* <Route path="/" element={<></>} /> */}
+            <Route path="/hospital" element={<Hospital />} />
+            <Route path="*" element={<></>} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "./assets/fontawesome/css/fontawesome.css";
-import "./assets/fontawesome/css/brands.css";
-import "./assets/fontawesome/css/solid.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import axios from "./axios"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
